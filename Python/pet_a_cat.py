19476c6515ffb995ce1ab0c9b2e0072a76aa2fa5
Jonathan Lee <jlee@broccoli.fios-router.home>
2020-09-02 11:58:05 -0400

    This one is larger, yet more useless

--- Python/pet_a_cat.py
+++ Python/pet_a_cat.py
@@ -1,10 +1,9 @@
-import random, time
-from threading import Timer
+from random import randint; from threading import Timer; from time import sleep
 def x(): globals().update(a=0); print('\nCat left you...')
 a = 1
 while a:
-    if random.randint(0,1):
-        time.sleep(random.randint(1,3))
-        t = Timer(1, x); t.start()
-        input('cat angry, pet it with <Enter>:')
+    if randint(0,1):
+        sleep(randint(1,3))
+        t = Timer(0.5, x); t.start()
+        input('cat angry, pet it with <Enter>')
         t.cancel()
